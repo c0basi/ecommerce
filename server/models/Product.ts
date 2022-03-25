09a70ded8e7e4aa2beb96f3a42ae1b7b5a77d03b
charles <charles.obasi@nuenergy.ai>
2022-03-24 23:40:41 -0400

    created Product schema

---
+++ server/models/Product.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from 'mongoose';
+interface Product {
+	title: string;
+	desc: string;
+	img: string;
+	categories: Array<String>;
+	size: string;
+	color: string;
+	price: number;
+}
+
+const ProductSchema = new Schema<Product>(
+	{
+		title: { type: String, required: true, unique: true },
+		desc: { type: String, required: true },
+		img: { type: String, required: true },
+		categories: { type: [String], required: true },
+		size: { type: String, required: true },
+		color: { type: String, required: true },
+		price: { type: Number, required: true },
+	},
+	{ timestamps: true }
+);
+
+export default mongoose.model('User', ProductSchema);
