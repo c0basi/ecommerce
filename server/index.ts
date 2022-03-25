efb383cc3f6e6d3f60f12f0c24a7bd8424ef2b89
charles <charles.obasi@nuenergy.ai>
2022-03-24 21:32:40 -0400

    adder initial server file

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,22 @@
-console.log('hello');
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const app: express.Application = express();
+
+const uri: string = process.env.DB_CONNECT!;
+
+mongoose
+	.connect(uri)
+	.then(() => console.log('Connected to database!!!'))
+	.catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.get('/', function (req: express.Request, res: express.Response) {
+	res.send('Hello World!');
+});
+
+app.listen(process.env.PORT || 5000, function () {
+	console.log(`starting app on: ${process.env.PORT}`);
+});
