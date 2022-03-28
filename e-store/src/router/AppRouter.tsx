c38d4d98da7fa80335b2f88851649bc2a3079743
charles <charles.obasi@nuenergy.ai>
2022-03-27 21:53:46 -0400

    added top level routes

--- e-store/src/router/AppRouter.tsx
+++ e-store/src/router/AppRouter.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Cart from '../pages/Cart';
 import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Product from '../pages/Product';
+import ProductList from '../pages/ProductList';
+import Register from '../pages/Register';
 
 const AppRouter = () => {
+	const user = true;
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
+			<Route path="products" element={<ProductList />} />
+			<Route path="products/:id" element={<Product />} />
+			<Route path="cart" element={<Cart />} />
+			<Route path="login" element={<Login />}></Route>
+			<Route path="register" element={<Register />} />
 		</Routes>
 	);
 };
