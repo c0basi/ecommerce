a2a61d343864e4ee0c93ce011db19286c2714e55
charles <charles.obasi@nuenergy.ai>
2022-04-01 18:08:26 -0400

    fixed login and router logic

--- e-store/src/pages/Login.tsx
+++ e-store/src/pages/Login.tsx
@@ -1,15 +1,52 @@
 import React from 'react';
+import { login } from '../redux/user/user-actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
 import './Login.scss';
+import { userSelector } from '../redux/user/userSlice';
 
 const Login = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+	const dispatch = useDispatch();
+	const { loading, error } = useSelector(userSelector);
+
+	const loginHandler = (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		dispatch(login({ username, password }));
+	};
+
 	return (
 		<div className="login-container">
 			<div className="login-container__wrapper">
 				<h1 className="login-container__wrapper--title">SIGN IN</h1>
-				<form action="" className="login-container__wrapper--form">
-					<input type="text" placeholder="username" />
-					<input type={'password'} placeholder="password" />
-					<button>LOGIN</button>
+				<form
+					action=""
+					className="login-container__wrapper--form"
+					onSubmit={loginHandler}
+				>
+					<input
+						className={`${error ? 'input-error' : ''}`}
+						type="text"
+						placeholder="username"
+						onChange={(e: React.FormEvent<HTMLInputElement>) =>
+							setUsername(e.currentTarget.value)
+						}
+					/>
+					<input
+						className={`${error ? 'input-error' : ''}`}
+						type={'password'}
+						placeholder="password"
+						onChange={(e: React.FormEvent<HTMLInputElement>) =>
+							setPassword(e.currentTarget.value)
+						}
+					/>
+					{error && (
+						<div className="error-text">Wrong username or password</div>
+					)}
+					<button type="submit" disabled={loading}>
+						LOGIN
+					</button>
 					<a href="">FORGOT PASSWORD?</a>
 					<a href="">CREATE A NEW ACCOUNT</a>
 				</form>
--- e-store/src/router/AppRouter.tsx
+++ e-store/src/router/AppRouter.tsx
@@ -7,16 +7,21 @@ import Product from '../pages/Product';
 import ProductList from '../pages/ProductList';
 import Register from '../pages/Register';
 import Success from '../pages/Success';
+import { userSelector } from '../redux/user/userSlice';
+import { useSelector } from 'react-redux';
 
 const AppRouter = () => {
-	const user = true;
+	const user = useSelector(userSelector).currentUser;
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="products/:category" element={<ProductList />} />
 			<Route path="product/:_id" element={<Product />} />
 			<Route path="cart" element={<Cart />} />
-			<Route path="login" element={<Login />}></Route>
+			<Route
+				path="login"
+				element={user ? <Navigate to="/" /> : <Login />}
+			></Route>
 			<Route path="register" element={<Register />} />
 			<Route path="success" element={<Success />}></Route>
 		</Routes>
