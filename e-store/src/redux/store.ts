95ed59814a8d673fd3b4712a652852f188638389
charles <charles.obasi@nuenergy.ai>
2022-04-01 18:21:26 -0400

    added persist for user login

--- e-store/src/redux/store.ts
+++ e-store/src/redux/store.ts
@@ -4,14 +4,35 @@ import cartReducer from './cart/cartSlice';
 import productReducer from './products/productSlice';
 import productsReducer from './products/productsSlice';
 import userReducer from './user/userSlice';
+import storage from 'redux-persist/lib/storage';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+
+const persistConfig = {
+	key: 'root',
+	version: 1,
+	storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 const store = configureStore({
 	reducer: {
 		cart: cartReducer,
 		product: productReducer,
 		products: productsReducer,
-		user: userReducer,
+		user: persistedReducer,
 	},
 });
+
+export let persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
