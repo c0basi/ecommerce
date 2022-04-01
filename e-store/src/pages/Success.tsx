cbf89ec09904aed52e88e9215307cc57a6471516
charles <charles.obasi@nuenergy.ai>
2022-04-01 03:25:33 -0400

    xreated success page

--- e-store/src/pages/Success.tsx
+++ e-store/src/pages/Success.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Success.scss';
 
 const Success = () => {
-	return <div>Success</div>;
+	return (
+		<div className="success-container">
+			<div className="success-container__items">
+				<div className="success-container__items--title">Success!</div>
+				<h3>
+					<Link to="/">Continue Shopping</Link>
+				</h3>
+			</div>
+		</div>
+	);
 };
 
 export default Success;
