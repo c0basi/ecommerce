e69b0203c1441ac587506f7fabae18057337566b
charles <charles.obasi@nuenergy.ai>
2022-04-18 23:18:55 -0400

    folder structure update

--- admin-panel/src/App.tsx
+++ admin-panel/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import Topbar from './components/topbar/topbar';
+import Home from './pages/home/Home';
 
 function App() {
-	return <div>Hello</div>;
+	return (
+		<div>
+			Hello
+			<Topbar />
+		</div>
+	);
 }
 
 export default App;
